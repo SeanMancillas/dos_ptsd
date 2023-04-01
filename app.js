e2d4e2c3da5e5f0bcec3c9d1601b16129adf93ae
Sean <sean@delighted.com>
2023-04-01 03:43:26 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,92 +1,92 @@
-const smellForm = document.getElementById('smellForm');
-const resultList = document.getElementById('resultList');
-const results = document.getElementById('results');
-const planContainer = document.getElementById('planContainer');
+document.getElementById("smellForm").addEventListener("submit", function (event) {
+    event.preventDefault();
 
-smellForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const selectedIndex = document.getElementById('smells').selectedIndex;
-    displayResults(selectedIndex);
-});
-
-const smellsAndScents = [
-    {
-        smell: "Gasoline",
-        scents: [
-            { scent: "Ethanol", similarity: 75 },
-            { scent: "Diesel", similarity: 90 },
-            { scent: "Kerosene", similarity: 80 },
-        ],
-    },
-    {
-        smell: "Cigarette Smoke",
-        scents: [
-            { scent: "Burnt Paper", similarity: 65 },
-            { scent: "Wood Smoke", similarity: 70 },
-            { scent: "Charred Food", similarity: 60 },
-        ],
-    },
-    {
-        smell: "Perfume",
-        scents: [
-            { scent: "Floral Scented Candle", similarity: 80 },
-            { scent: "Scented Lotion", similarity: 85 },
-            { scent: "Air Freshener", similarity: 75 },
-        ],
-    },
-    {
-        smell: "Cleaning Products",
-        scents: [
-            { scent: "Lemon", similarity: 70 },
-            { scent: "Pine", similarity: 80 },
-            { scent: "Lavender", similarity: 65 },
-        ],
-    },
-    // Add more smells and scents objects here
-];
-
-function displayResults(index) {
-    const selectedSmell = smellsAndScents[index - 1];
-    resultList.innerHTML = '';
+    const selectedSmell = document.getElementById("smells").value;
 
     if (selectedSmell) {
-        for (const [i, scent] of selectedSmell.scents.entries()) {
-            const listItem = document.createElement('li');
-            listItem.textContent = `${scent.scent} - Similarity: ${scent.similarity}% - `;
+        document.getElementById("results").style.display = "block";
+        const resultList = document.getElementById("resultList");
 
-            const badge = document.createElement('span');
-            badge.classList.add('ai-customized-plan');
-            badge.textContent = 'AI Customized Plan';
-            badge.addEventListener('click', () => displayCustomizedPlan(i));
-            listItem.appendChild(badge);
+        // Display sample data for demo purposes
+        resultList.innerHTML = `
+            <li><span class="ai-customized-plan">AI Customized Plan</span> Diesel <span class="trending">(Trending with community)</span></li>
+            <li><span class="ai-customized-plan">AI Customized Plan</span> Gasoline</li>
+            <li><span class="ai-customized-plan">AI Customized Plan</span> Kerosene</li>
+        `;
 
-            if (i === 1) {
-                const trendingText = document.createElement('span');
-                trendingText.classList.add('trending');
-                trendingText.textContent = 'Trending with community';
-                listItem.appendChild(trendingText);
-            }
-
-            resultList.appendChild(listItem);
+        // Display Diesel AI Customized Plan if selected smell is "Gasoline"
+        if (selectedSmell === "1") {
+            const planContainer = document.getElementById("planContainer");
+            planContainer.innerHTML = `
+                <h2>AI Customized Plan: Diesel</h2>
+                <h3>Where to purchase the scent:</h3>
+                <p><a href="https://www.amazon.com/Boostnatics-Diesel-Fuel-Scented-Candle/dp/B06XDNBX69">Boostnatics Diesel Fuel Scented Candle [$23.95]</a></p>
+                <h3>How to test in a safe way:</h3>
+                <ol>
+                    <li>Find a quiet, comfortable, and well-ventilated space where you can practice the exposure exercise.</li>
+                    <!-- Add the rest of the steps here -->
+                </ol>
+                <button onclick="startExposure()">Start the exposure</button>
+            `;
         }
     }
+});
 
-    results.style.display = 'block';
+function startExposure() {
+    document.getElementById("timerSection").style.display = "block";
+    startTimer();
 }
 
-function displayCustomizedPlan(index) {
-    // Update the contents of the planContainer based on the selected scent
-    planContainer.innerHTML = `
-        <h2>AI Customized Plan</h2>
-        <p><strong>Where to purchase the scent:</strong> [Insert information here]</p>
-        <p><strong>Step-by-step instructions for safe testing:</strong></p>
-        <ol>
-            <li>[Step 1]</li>
-            <li>[Step 2]</li>
-            <li>[Step 3]</li>
-            <!-- Add more steps as needed -->
-        </ol>
-    `;
+let timerInterval;
+let remainingSeconds = 5 * 60;
 
-    planContainer.style.display = 'block';
+function startTimer() {
+    timerInterval = setInterval(function () {
+        remainingSeconds--;
+
+        const minutes = Math.floor(remainingSeconds / 60);
+        const seconds = remainingSeconds % 60;
+
+        document.getElementById("timer").textContent = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+
+        if (remainingSeconds === 0) {
+            clearInterval(timerInterval);
+            playBeeps();
+            showFeedbackModal();
+        }
+    }, 1000);
+}
+
+function pauseResumeTimer() {
+    const pauseResumeBtn = document.getElementById("pauseResumeBtn");
+    if (pauseResumeBtn.textContent === "Pause") {
+        clearInterval(timerInterval);
+        pauseResumeBtn.textContent = "Resume";
+    } else {
+        startTimer();
+        pauseResumeBtn.textContent = "Pause";
+    }
+}
+
+function resetTimer() {
+    clearInterval(timerInterval);
+    remainingSeconds = 5 * 60;
+    document.getElementById("timer").textContent = "05:00";
+    document.getElementById("pauseResumeBtn").textContent = "Pause";
+}
+
+function playBeeps() {
+    const beep = new Audio("beep.mp3");
+    beep.play();
+    setTimeout(() => beep.play(), 1000);
+    setTimeout(() => beep.play(), 2000);
+}
+
+function showFeedbackModal() {
+    document.getElementById("feedbackModal").style.display = "block";
+}
+
+function submitFeedback() {
+    document.getElementById("feedbackModal").style.display = "none";
+    // Process the feedback data as needed
 }
