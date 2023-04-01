dc1f3a0408b6d2b366b08ba28d5d443161e9b60c
Sean <sean@delighted.com>
2023-04-01 03:15:26 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const smellForm = document.getElementById('smellForm');
 const resultList = document.getElementById('resultList');
 const results = document.getElementById('results');
+const planContainer = document.getElementById('planContainer');
 
 smellForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -49,12 +50,43 @@ function displayResults(index) {
     resultList.innerHTML = '';
 
     if (selectedSmell) {
-        for (const scent of selectedSmell.scents) {
+        for (const [i, scent] of selectedSmell.scents.entries()) {
             const listItem = document.createElement('li');
-            listItem.textContent = `${scent.scent} - Similarity: ${scent.similarity}% - Plan: [Customized plan]`;
+            listItem.textContent = `${scent.scent} - Similarity: ${scent.similarity}% - `;
+
+            const badge = document.createElement('span');
+            badge.classList.add('ai-customized-plan');
+            badge.textContent = 'AI Customized Plan';
+            badge.addEventListener('click', () => displayCustomizedPlan(i));
+            listItem.appendChild(badge);
+
+            if (i === 1) {
+                const trendingText = document.createElement('span');
+                trendingText.classList.add('trending');
+                trendingText.textContent = 'Trending with community';
+                listItem.appendChild(trendingText);
+            }
+
             resultList.appendChild(listItem);
         }
     }
 
     results.style.display = 'block';
+}
+
+function displayCustomizedPlan(index) {
+    // Update the contents of the planContainer based on the selected scent
+    planContainer.innerHTML = `
+        <h2>AI Customized Plan</h2>
+        <p><strong>Where to purchase the scent:</strong> [Insert information here]</p>
+        <p><strong>Step-by-step instructions for safe testing:</strong></p>
+        <ol>
+            <li>[Step 1]</li>
+            <li>[Step 2]</li>
+            <li>[Step 3]</li>
+            <!-- Add more steps as needed -->
+        </ol>
+    `;
+
+    planContainer.style.display = 'block';
 }
